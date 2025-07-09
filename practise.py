2125599ac8d83957e869d25a952d0170dc4e03cc
Adityakiran <menonadityakiran@gmail.com>
2025-07-09 09:32:03 +0200

    Delete practise.py

--- practise.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-print("Hello,", name, "! Wow,", age, "is a great age!")
-
-output : 
-What is your name? sarah
-How old are you? 17
-Hello, sarah ! Wow, 17 is a great age!
